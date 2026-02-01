ba9139f6db34c6d4d0ce393aedae291a274adeaa
Yuk <86674283+37saku@users.noreply.github.com>
2026-02-01 20:34:31 +0800

    Refactor index.php with updated title and layout
    Updated the title and modified various sections including navigation, social links, and contact information. Removed unused CSS styles and added a new stylesheet link.

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | sanayuk</title>
+    <link rel="icon" type="image/jpeg" href="snypfp.jpg">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <img src="snypfp.jpg" alt="Profile Picture" class="profile-pic">
+        
+        <br>
+        <a href="about.php" class="name-link">
+        <h1 data-text="about me">sanayuk</h1>
+        </a>
+
+        <br>
+        <nav class="site-nav">
+        <a href="index.php" class="contact-link">Home</a> | 
+        <a href="about.php">About</a> | 
+        <a href="recents.php">Recents</a> | 
+        <a href="socials.php">Socials</a>
+    </nav>
+
+        <h3>YouTube</h3>
+        <div class="links">
+            <a href="https://www.youtube.com/@sanayuk____" class="link-card" target="_blank">sanayuk | personal</a>
+            <a href="https://www.youtube.com/@loft2." class="link-card" target="_blank">loft2 | fancams</a>
+        </div>
+
+        <h3 >Active on</h3>
+        <div class="links">
+            <a href="https://www.instagram.com/sanayuk____/" class="link-card" target="_blank">Instagram</a>
+            <a href="https://www.threads.net/@sanayuk____/" class="link-card" target="_blank">Threads</a>
+        </div>
+
+        <h3>Latest Video</h3>
+        <div class="video-container">
+            <iframe 
+                src="https://www.youtube-nocookie.com/embed/hS_lLDnEj5Q" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen>
+            </iframe>
+        </div>
+    
+        <h3 class="tight-header">Contact Me</h3>
+        <div class="contact-line">
+            <a href="mailto:sanayuuuk@gmail.com?subject=Contact from Website" class="contact-link">
+                sanayuuuk@gmail.com
+            </a>
+            <span class="separator">|</span>
+            <a href="https://www.instagram.com/sanayuk____/" class="contact-link" target="_blank">
+                @sanayuk____
+            </a>
+        </div>
+
+        <footer>
+            <p>Website created: 2023/09/15 18:40 (UTC +8)</p>
+            <p>Last Updated: 2026/02/01 20:32 (UTC +8)</p>
+            <p>© 2026 sanayuk. All rights reserved.</p>
+        </footer>
+    </div>
+
+</body>
+</html>
